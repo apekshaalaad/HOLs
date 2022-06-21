2299f11a19b0f5587943a81d98a92f20bb623d73
apekshaalaad <apekshalaad@gmail.com>
2022-06-21 10:29:10 +0530

    fifth commit

---
+++ Day6MVCAssign/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Day6MVCAssign.Models;
+
+namespace Day6MVCAssign.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        static List<User> users = new List<User>();
+        public ActionResult GetServerTime()
+        {
+            System.Threading.Thread.Sleep(5000);
+            return PartialView();
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult GetAllUsers()
+        {
+            return PartialView(users);
+        }
+        public ActionResult CreateUser()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreateUser(User u)
+        {
+            users.Add(u);
+            return RedirectToAction("GetAllUsers");
+        }
+
+    }
+}
