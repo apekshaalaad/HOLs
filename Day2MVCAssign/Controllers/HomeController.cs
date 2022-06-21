bf6640d5aa42733b4fcb5771b71beba6727e33ae
apekshaalaad <apekshalaad@gmail.com>
2022-06-21 10:09:51 +0530

    first commit

---
+++ Day2MVCAssign/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Day2MVCAssign.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index(string id)
+        {
+            if (id == null)
+            {
+                return Content("<h1>This is a demo of ContentResult</h1>");
+              
+            }
+            else if (id.ToLower() == "plain")
+            {
+                return Content("<h1>This is a demo of ContentResult</h1 >", "text/plain");
+
+            }
+            else if (id.ToLower() == "html")
+            {
+                return Content("<h1>This is a demo of ContentResult</h1>", "text/html");
+
+            }
+            else if (id.ToLower() == "xml")
+            {
+                return Content("<h1>This is a demo of ContentResult</h1>", "text/xml");
+
+            }
+            return Content("Invalid content type");
+        }
+        public ActionResult DownloadFile()
+        {
+            if (!System.IO.File.Exists(@"E:\Images\Screenshot24.png"))
+            {
+                return Content("File to be downloaded not found");
+            }
+            return File(@"E:\Images\Screenshot24.png", "image/jpg",
+           "default.jpg");
+        }
+        public ActionResult Google()
+        {
+            return Redirect("http://www.google.com");
+        }
+        public ActionResult StartPage()
+        {
+            return RedirectToAction("Index", new { id = "xml" });
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult AboutUs()
+        {
+            return View();
+        }
+
+    }
+}
